a98dbb0974814439297f4a162f6a8ba811eec534
Christian Talmo <talmo.christian@gmail.com>
2016-05-17 12:03:41 +0300

    rewrote library -> consistent chart plotting

--- realtimechart.js
+++ realtimechart.js
@@ -1,255 +1,175 @@
-function init (canvas, opts) {
-  var log = function (s) {
-    //console.log(s)
-  }
-
-  var canvas = (typeof canvas === 'string') ? document.getElementById(canvas) : canvas
-  if (!canvas) return
+function init(canvas, params) {
+  if (typeof canvas === 'string') canvas = document.getElementById(canvas)
   var ctx = canvas.getContext('2d')
-  var api = null
-  var timeout = null
 
-  var defaults = {
-    width: 300,
-    height: 40
+  params = params || {}
+  params.width = params.width || 300
+  params.height = params.height || 40
+
+
+  canvas.width = params.width
+  canvas.height = params.height
+
+  var interval = params.interval || 10000
+
+  // add initial data if available
+  var data = params.data || []
+  data.sort(function (a, b) {
+    if (a.time < b.time) return -1
+    if (a.time > b.time) return 1
+    return 0
+  })
+
+  var seed = 1
+  function random () {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x)
   }
 
-  var opts = opts || {}
+  var ticks = 0
+  var tpp = (interval / canvas.width) | 0
 
-  var init = function (canvas, initData) {
+  // add initial fake data
+  //for (var i = 0; i < 100; i++) {
+  //  data.push({
+  //    value: 10 + random() * 20,
+  //    time: Date.now() - tpp * 10 * i
+  //  })
+  //}
+  //data.reverse()
+
+  function clear () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
 
-    var __running = true
+  function drawBorder () {
+    ctx.strokeStyle = '#000'
+    ctx.strokeRect(0, 0, canvas.width, canvas.height)
+  }
 
-    var w = canvas.width
-    var h = canvas.height
+  function tti (time) {
+    var delta = Date.now() - time
+    return delta / tpp
+  }
 
-    var offset = typeof opts.offset === 'number' ? opts.offset : 1000 // in ms
+  // group values near each other as one
+  function compressData () {
+    var newData = []
+    var time = data[0]
+    var sum = 0
+    var count = 0
+    for (var i = 0; i < data.length; i++) {
+      var o = data[i]
 
-    var ticks = 0
-    var line_width = opts.line_width || 8
+      if (o.time <= time) {
+        sum += o.value
+        count++
+      } else {
+        newData.push({
+          time: time,
+          value: sum / count
+        })
 
-    ctx.fillRect(0, 0, w, h)
-
-    function clear () {
-      ctx.clearRect(0,0,w,h)
-    }
-
-    clear()
-
-    function drawBorder () {
-      ctx.strokeStyle = "black"
-      ctx.strokeRect(0,0,w,h)
-    }
-
-    var scale = function (min, max) {
-      return function (val) {
-        return Math.max(0.0, Math.min(1.0, ((min + val) / max)))
+        sum = o.value
+        count = 1
+        time = o.time
       }
     }
+    newData.push({
+      time: time,
+      value: sum / count
+    })
 
-    var yScale = scale(0, h)
+    data = newData
+  }
 
-    var interval = opts.interval || 10000 // ms
-    var pps = canvas.width / (interval / 1000) // pixels per second
-
-    var tpp = interval / canvas.width // time per pixel (= 50ms)
-
-    // right offset
-    var offset_len = Math.ceil((offset / tpp) / line_width) + 1
-
-    // left offset, smoothes out exits
-    var buf_left = 1
-
-    var data = []
-    for (var i = 0; i < Math.ceil(((canvas.width / line_width) + buf_left) + offset_len); i++) {
-      var v = (i * 2) % 10
-      v = 0
-      data.push({value: v, time: tpp * i })
-    }
-
-    if (initData && (initData instanceof Array) && initData.length) {
-      log('------------- INIT DATA FOUND --------------')
-      log('init data length: %s', initData.length)
-      var _id = initData.slice(initData.length - data.length)
-      var len = Math.min(initData.length, data.length)
-      for (var i = 0; i < len; i++) {
-        var d = _id[i]
-        //addValue(d.value, d.time)
-        data.push(d)
-        data.shift()
-      }
-    }
-
-    log('data.length at init: %s', data.length)
-
-    var timeToIndex = function (time) {
-      var delta = (Date.now() - time)
-      var index = Math.floor((delta / tpp) / line_width)
-      //log("index: " + index)
-      return index
-    }
-
-    var TICKS_PER_SECOND = pps
-    var MS_PER_TICK = 1000 / TICKS_PER_SECOND
-
-    var startTime = Date.now()
+  function plotData () {
+    ctx.strokeStyle = '#394'
 
     var origin = {
-      x: w,
-      y: h,
+      x: canvas.width,
+      y: canvas.height
     }
 
-    function addValue (value, time) {
-      time = time || Date.now()
-      value = value || 0
+    // apply offset
+    if (params.offset) origin.x += (params.offset / tpp)
 
-      var index = timeToIndex(time)
-      var t = data.length - index - 1
-      if (t < data.length && t >= 0) {
-        data[t].value += value
-      }
-    }
+    ctx.beginPath()
 
-    function render () {
-      var xoff = ticks % line_width
-      xoff -= line_width
+    if (params.bound) ctx.moveTo(origin.x, origin.y)
 
-      var d = data.slice(0, data.length - offset_len + buf_left).reverse()
+    for (var i = 1; i < data.length; i++) {
+      var d = data[data.length - i]
+        var value = d.value
+        var time = d.time - (d.time % tpp)
 
-      ctx.strokeStyle = "green"
-      // draw all data points
-      ctx.beginPath()
-      ctx.moveTo(origin.x - xoff, origin.y)
-
-      // calculate new ySclale (based around max value)
-      var yMax = 1
-      for (var i = 0; i < d.length; i++) {
-        var v = Number(d[i].value)
-        if (v > yMax) {
-          yMax = v
-        }
-      }
-      //log("ymax: %s", yMax)
-      yScale = scale(0, yMax * 1.25)
-
-      for (var i = 0; i < d.length; i++) {
-        var point = d[i]
         var to = {
-          x: origin.x - i * line_width,
-          y: origin.y - point.value,
+          x: origin.x - tti(time),
+          y: canvas.height - value * .5
         }
-
-        to.y = origin.y - yScale(point.value) * h
-
-        ctx.lineTo(to.x - xoff, to.y)
-      }
-
-      ctx.stroke()
-
+      ctx.lineTo(to.x, to.y)
     }
 
-    function tick () {
-      ticks++
-      clear()
-
-      if (ticks % line_width === 0) {
-        data.push({value: 0, time: Date.now()})
-        data.shift()
-      }
-
-      render()
-      drawBorder()
-
-      if (__running) {
-        timeout = setTimeout(tick, MS_PER_TICK)
-      }
-    }
-
-    tick()
-
-    log('get end len: %s', data.length)
-
-    return {
-      addData: function (value, time) {
-        addValue(value, time) // epochTime
-      },
-      getData: function () {
-        return data.slice()
-      },
-      data: data,
-      stop: function () {
-        __running = false
-      }
-    }
-  } // init
-
-  function resize (params) {
-    var opts = Object.assign({}, defaults, params)
-    log('resizing: %sx%s', opts.width, opts.height)
-
-    if (params.size === 'auto') {
-      log('------------ IS AUTO ----------')
-      opts.width = 'auto'
-      opts.height = 'auto'
-    }
-
-    canvas.width = opts.width
-    canvas.height = opts.height
-
-    log('cw: %s, ch: %s', canvas.width, canvas.height)
-
-    canvas.style.width = canvas.style.height = 'auto'
-
-    if (opts.width === 'auto') {
-      canvas.style.width = "100%"
-      canvas.width = canvas.offsetWidth || 300
-      log('offsetWidth: %s', canvas.offsetWidth)
-    }
-
-    if (opts.height === 'auto') {
-      canvas.style.height = "100%"
-      canvas.height = canvas.offsetHeight || 40
-      canvas.style.marginTop = "4px"
-      log('offsetHeight: %s', canvas.offsetHeight)
-    }
-  }
-  resize(opts)
-
-  var _opts = opts
-  setTimeout(function () {
-    resize(opts)
-    update()
-  }, 0)
-
-  var update = function () {
-    var _d = api.getData()
-    api.stop()
-    clearTimeout(timeout)
-    log('UPDATE LENGTH: %s', _d.length)
-    var _api = init(canvas, _d)
-    api.addData = _api.addData
-    api.stop = _api.stop
-    api.getData = _api.getData
+    ctx.stroke()
   }
 
-  var _api = init(canvas)
-  api = {
-    addData: _api.addData,
-    getData: function () {
-      return _api.getData()
-    },
-    stop: _api.stop,
-    update: function () {
-      return update()
-    },
-    resize: function (opts) {
-      resize(opts || {width: 'auto', height: 'auto'})
-      update()
-    }
+  function render () {
+    clear()
+    console.log('before compress lenth: %s', data.length)
+    compressData()
+    console.log('after compress length: %s', data.length)
+    plotData()
+    console.log('plot length: %s', data.length)
+    drawBorder()
   }
 
-  return api
+  function insertData (value, time) {
+    var o = {
+      value: value,
+      time: time
+    }
+
+    for (var i = data.length - 1; i > 0; i--) {
+      var d = data[i]
+      if (time > d.time) {
+        data.splice(i + 1, 0, o)
+        return
+      }
+    }
+
+    data.push(o)
+  }
+
+  function tick () {
+    var now = Date.now()
+
+    ticks++
+
+    if (ticks % tpp === 0) {
+      if (params.flat) insertData(1, now + tpp * tpp)
+    }
+    //insertData(random() * 25, now + 500 * random())
+
+    var index = 0
+    while (data[index] && data[index].time < (now - interval * 1)) {
+      index++
+    }
+    data = data.slice(Math.max(index - 2, 0))
+
+    render()
+    setTimeout(tick, tpp)
+  }
+  tick()
+
+  render()
+
+  var ctrl = {
+    insertData: insertData
+  }
+
+
+  return ctrl
 }
 
-if (typeof module !== 'undefined' && module.exports) moduel.exports = init
+module = module || {}
+module.exports = init
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var rtc = require('../realtimechart.js')
+
+//var canvas = document.createElement('canvas')
+
+rtc(null)
